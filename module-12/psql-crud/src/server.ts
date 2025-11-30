7e4445da8f9e5628d555abdb553a4d673c9db262
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-12-01 00:05:48 +0600

    table created

--- module-12/psql-crud/src/server.ts
+++ module-12/psql-crud/src/server.ts
@@ -1,6 +1,49 @@
 import express, { Request, Response } from 'express'
+import {Pool} from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({path: path.join(process.cwd(), '.env' )});
+
 const app = express()
 const port = 5000
+
+// PostgreSQL connection pool - NeonDB
+const pool = new Pool({
+    connectionString: `${process.env.CONNECTION_STR}`,
+})
+
+const initDB = async()=> {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        email VARCHAR(50) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        
+        `);
+
+         await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+
+
+};
+
+initDB();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
